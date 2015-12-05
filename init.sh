359bf73b5bfa9fd4e82a232a8f0d2252e5776fce
Yusuke IWASAWA <iwasawa@weblab.t.u-tokyo.ac.jp>
2015-12-05 18:51:20 +0900

    [Add] gitの初期化周りの処理をスクリプト化

---
+++ init.sh
@@ -0,0 +1,8 @@
+git submodule init .vim/bundle/neobundle.vim
+git submodule update .vim/bundle/neobundle.vim
+git submodule init .zsh/zsh-completions
+git submodule update .zsh/zsh-completions
+git submodule init .zsh/plugins/zaw
+git submodule update .zsh/plugins/zaw
+git config --global user.email "iwasawa@weblab.t.u-tokyo.ac.jp"
+git config --global user.name "Yusuke IWASAWA"
